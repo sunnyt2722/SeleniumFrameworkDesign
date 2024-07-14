9a84710676c3fba6328878f77412846f8d7bac76
sunnyt2722 <sunnyt135@gmail.com>
2024-07-14 02:40:50 +0100

    Initial Commit

---
+++ src/test/java/sunnykumarlearning/StandAloneTest.java
@@ -0,0 +1,31 @@
+package sunnykumarlearning;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class StandAloneTest {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://rahulshettyacademy.com/client");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("userEmail")).sendKeys("sunnykumar@leaning.com");
+		driver.findElement(By.id("userPassword")).sendKeys("Test@123");
+		driver.findElement(By.id("login")).click();
+		
+		List<WebElement> products = driver.findElements(By.className(".mb-3"));
+
+		WebElement prod = products.stream().filter(product->product.findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3")).findFirst().orElse(null);
+		
+		prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+	}
+}
